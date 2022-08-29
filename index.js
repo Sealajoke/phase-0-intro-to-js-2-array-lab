26b6a6729d9fc233eac666d0ca49e026875c4363
seunaliu3@gmail.com <seunaliu3@gmail.com>
2022-08-29 23:40:30 +0100

    commit tree ef4c7829f104ea4fb686efa0aca900050110c2fa

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+//function cats(){["Milo", "Otis", "Garfield"]}
+
+var cats = ["Milo", "Otis", "Garfield"];
+cats;
+
+var cats = ["Milo", "Otis", "Garfield"];
+cats.push("Ralph");
+cats;
+
+
+
